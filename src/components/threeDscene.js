61d52c4bdc87070560a7c779c0f87b7ed8d9871f
esben holk nielsen <esbenholknielsen@gmail.com>
2021-12-08 12:10:13 +0100

    three installed

--- src/components/threeDscene.js
+++ src/components/threeDscene.js
@@ -38,12 +38,12 @@ function Boxes(imageUrl) {
       for (let y = 0; y < 10; y++)
         for (let z = 0; z < 10; z++) {
           const id = i++;
-          tempObject.position.set(5 - x, 5 - y, 5 - z);
+          tempObject.position.set(50 - x, 50 - y, 50 - z);
           tempObject.rotation.y =
             Math.sin(x / 4 + time) +
             Math.sin(y / 4 + time) +
             Math.sin(z / 4 + time);
-          tempObject.rotation.z = tempObject.rotation.y * 2;
+          tempObject.rotation.z = tempObject.rotation.y * 1;
           if (hovered !== prevRef.Current) {
             tempColor
               .set(id === hovered ? "white" : data[id].color)
